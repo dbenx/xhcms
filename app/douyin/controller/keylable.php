241f7ae5287dfc84add5c704af4055053a217865
dbenx <331836707@qq.com>
2022-12-19 16:03:30 +0800

    更新注释

--- app/douyin/controller/keylable.php
+++ app/douyin/controller/keylable.php
@@ -8,12 +8,67 @@ use think\admin\helper\QueryHelper;
 
 class keylable extends Controller
 {
-    public  function index(){
+    /**
+     * 标签管理
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
+    public function index()
+    {
         DouyinKeylable::mQuery()->layTable(function () {
             $this->title = '标签管理';
+
         }, function (QueryHelper $query) {
             $query->like('title');
         });
     }
 
+    /**
+     * 添加信息
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
+    public function add()
+    {
+        DouyinKeylable::mForm('form');
+    }
+
+    /**
+     * 修改信息
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
+    public function edit()
+    {
+        DouyinKeylable::mForm('form');
+    }
+
+
+    protected function _form_filter(array &$data)
+    {
+        if ($this->request->isPost()) {
+            [$state, $rep, $error] = [0, 0, 0];
+            if ($data['name'] == '') $this->error('数据不能为空！');
+            $url = explode(PHP_EOL, $data['name']);
+            foreach ($url as $vo) {
+                if ($vo != '') {
+                    if (DouyinKeylable::mk()->where(['title' => $vo, 'uid' => session('user.id'), 'is_deleted' => 0])->field('id')->count() > 0) {
+                        $rep++;
+                    } else {
+                        $insdata[] = array(
+                            'uid' => session('user.id'),
+                            'title' => trim($vo)
+                        );
+                    }
+                }
+            }
+            if (!empty($insdata)) {
+                $state = DouyinKeylable::mk()->insertAll($insdata);
+            }
+            $this->success('本次更新' . $state . '条信息，重复信息' . $rep . '条,错误' . $error . '条');
+        }
+    }
 }
--- app/douyin/controller/video.php
+++ app/douyin/controller/video.php
@@ -2,15 +2,24 @@
 
 namespace app\douyin\controller;
 
+use app\douyin\model\DouyinKeylable;
 use app\douyin\model\DouyinVideo;
 use think\admin\Controller;
 use think\admin\helper\QueryHelper;
-use think\admin\model\SystemUser;
 use think\facade\Config;
 
 class video extends Controller
 {
 
+    /**
+     * 视频管理中心
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public function index()
     {
         DouyinVideo::mQuery()->layTable(function () {
@@ -20,12 +29,24 @@ class video extends Controller
         });
     }
 
+    /**
+     * 添加信息
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
     public function add()
     {
         $this->marks = Config::get('source')['region'];
         DouyinVideo::mForm('form');
     }
 
+    /**
+     * 编辑信息
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
     public function edit()
     {
         $this->marks = Config::get('source')['region'];
@@ -35,26 +56,55 @@ class video extends Controller
     protected function _form_filter(array &$data)
     {
         if ($this->request->isPost()) {
-            if(isset($data['region'])){
+            if (!isset($data['key_label'])) $this->error('请选择关键词标签');
+            if (isset($data['region'])) {
                 $data['region'] = arr2str($data['region']);
-            }else{
-                $data['region'] ='';
+            } else {
+                $data['region'] = '';
             }
+            $data['key_label'] = arr2str($data['key_label']);
         } else {
+            $this->lables = DouyinKeylable::mk()->where(['uid' => session('user.id'), 'status' => 1, 'is_deleted' => 0])->column('id,title');
+            $data['region'] = str2arr($data['region']);
 
-            $data['region']=str2arr($data['region']);
         }
     }
 
     /**
-     * 修改用户状态
-     * @auth true
+     * 表单结果处理
+     * @param bool $state
+     * @return void
+     */
+    protected function _form_result(bool $state)
+    {
+        if ($state) {
+            $this->success('数据更新成功', "javascript:history.back()");
+        }
+    }
+
+    /**
+     * 修改状态
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
      */
     public function state()
     {
         DouyinVideo::mSave($this->_vali([
-            'status.in:0,1'  => '状态值范围异常！',
+            'status.in:0,1' => '状态值范围异常！',
             'status.require' => '状态值不能为空！',
         ]));
+    }
+
+
+    /**
+     * 删除视频
+     * @auth true  # 表示需要验证权限
+     * @menu true  # 添加系统菜单节点
+     * @login true # 强制登录才可访问
+     */
+    public function remove()
+    {
+        DouyinVideo::mDelete();
     }
 }
